a22de2655f3aea38b090a636630278aaefbb1bae
Amit Samadder <amiamitswe@gmail.com>
2023-03-29 11:33:03 +0600

    api post request

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function CustomerComments() {
   const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
 
   const fetchCommentsHandler = async () => {
     const response = await fetch("/api/comments");
@@ -10,8 +11,29 @@ function CustomerComments() {
     console.log(data);
     setComments(data);
   };
+
+  const postCommentHandler = async () => {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div>
+      <div>
+        <input
+          type="text"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <button onClick={postCommentHandler}>Save Comments</button>
+      </div>
       <button onClick={fetchCommentsHandler}>Fetch Data</button>
 
       {comments.map((comment) => (
